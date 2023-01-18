2d516ddf0fb214299145a669b11d011394eea7ee
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-01-18 11:33:38 +0100

    Everything is ready

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -48,6 +48,12 @@ const imageUrl =[
 
     const [articles, setArticles] = useState<Articles[]>([]);
     const [sections, setSections] = useState<string[]>([]);
+    const [expandedSections, setExpandedSections] = useState<Record<string, boolean>>(
+        sections.reduce((acc, section) => {
+          acc[section] = true;
+          return acc;
+        }, {} as Record<string, boolean>)
+      );
 
       useEffect(() => {
         const api = async () => {
@@ -66,31 +72,44 @@ const imageUrl =[
             setSections(Array.from(new Set(tags)));
         }
     }, [articles]);
+
+    
     
 
     return (
-        <div className='section-container'>
-              { sections.map(section => (
-                <div className="section" key={section}>
-                    <h2>{section.toUpperCase()}</h2>
-                    <div className='cards-container'>
-                      {articles.map(article => (
-                            article.tags.includes(section) && (
-                                <div key={article.id} className='card'>
-                                    <img className='cart-image' src={imageUrl.find(img => img.title === article.title)?.url} width='250' height='150'/>
-                                    <div className='card-title'>{article.title}</div>
-                                    <div className='card-body'>{article.body}</div>
-                                    <div className='card-tags'>{article.tags.map((tag) => (<span className='tag'>{tag.toUpperCase()}</span>))}</div>
-                                </div>
-                            )
+        <div className="section-container">
+          {sections.map((section) => {
+            const isExpanded = expandedSections[section];
+            return (
+              <div className="section" key={section}>
+                <h2>
+                  {section.toUpperCase()}
+                  <button onClick={() => setExpandedSections({ ...expandedSections, [section]: !isExpanded })}>
+                    {isExpanded ? "↓" : "→"}
+                  </button>
+                </h2>
+                {isExpanded && (
+                  <div className="cards-container">
+                    {articles
+                      .filter((article) => article.tags.includes(section))
+                      .map((article) => (
+                        <div key={article.id} className="card">
+                          <img className="card-image" src={imageUrl.find((img) => img.title === article.title)?.url} width="350" height="250"/>
+                          <div className="card-title">{article.title}</div>
+                          <div className="card-body">{article.body}</div>
+                          <div className="card-tags">
+                            {article.tags.map((tag) => (
+                              <span className="tag">{tag.toUpperCase()}</span>
+                            ))}
+                          </div>
+                        </div>
                       ))}
-                    </div>
-                </div>
-              ))}
+                  </div>
+                )}
+              </div>
+            );
+          })}
         </div>
-);
-
-
-
-
+      );
+    
 }
