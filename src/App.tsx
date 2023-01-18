d9830d627930703cc93e3ff3b2dd8ca6cd15d60c
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-01-18 12:12:51 +0100

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import Blog from './components/Blog'
+import logo from './logo.png'
 
 function App() {
   return (
     <div>
       <header>
       </header>
-        <h1>Jak's blog</h1>
+        <img src={logo} alt="logo" className="logo"/>
         <Blog/>
     </div>
   );
 }
 
 export default App;
+
